f03eb3a4a5065d4c98694e43a651a60e3fde4f9a
Ben White <whtouche@gmail.com>
2014-06-10 14:36:20 -0400

    create two new files

---
+++ simple_hash.rb
@@ -0,0 +1,18 @@
+# Create a hash containing key-value pairs for the new england states.
+ne_states = {'MA' => 'Massachusetts', 'NH' => 'New Hampshire', 'ME' => 'Maine',
+ 'RI' => 'Rhode Island', 'CONN' => "Connecticut", 'VT' => 'Vermont'}
+
+# How many entries are in the ne_states hash?
+# 6
+
+puts ne_states['VT']
+puts ne_states['RI']
+
+squares = { 4 => 16, 5 => 25, 7 => 49}
+
+puts squares
+
+ne_states = {ma: 'Massachusetts', nh: 'New Hampshire', me: 'Maine',
+ ri: 'Rhode Island', ct: "Connecticut", vt: 'Vermont'}
+
+puts ne_states
